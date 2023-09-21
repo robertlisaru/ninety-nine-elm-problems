75094136140970bc5d77c05a67f64ee98496c527
robertlisaru <lisaru.robert@gmail.com>
2023-09-21 13:38:40 +0300

    Use String.join for listToString

--- src/Utils.elm
+++ src/Utils.elm
@@ -3,12 +3,7 @@ module Utils exposing (listToString, maybeToString)
 
 listToString : (a -> String) -> String -> List a -> String
 listToString convert separator list =
-    "["
-        ++ (list
-                |> List.map convert
-                |> List.intersperse separator
-                |> List.foldr (++) "]"
-           )
+    "[" ++ (list |> List.map convert |> String.join separator) ++ "]"
 
 
 maybeToString : (a -> String) -> Maybe a -> String
