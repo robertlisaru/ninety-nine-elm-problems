ab5dca2b07ee26b7cc4c751084e6cfdd3c825aa5
robertlisaru <lisaru.robert@gmail.com>
2023-10-15 18:03:04 +0300

    Refactor P34 solution

--- src/Solutions/P34Totient.elm
+++ src/Solutions/P34Totient.elm
@@ -3,18 +3,20 @@ module Solutions.P34Totient exposing (totient)
 
 totient : Int -> Int
 totient m =
-    let
-        coprimes x y =
-            let
-                gcd a b =
-                    if b == 0 then
-                        abs a
-
-                    else
-                        gcd b (a |> remainderBy b)
-            in
-            gcd x y == 1
-    in
     List.range 1 m
         |> List.filter (coprimes m)
         |> List.length
+
+
+coprimes : Int -> Int -> Bool
+coprimes x y =
+    gcd x y == 1
+
+
+gcd : Int -> Int -> Int
+gcd a b =
+    if b == 0 then
+        abs a
+
+    else
+        gcd b (a |> remainderBy b)
