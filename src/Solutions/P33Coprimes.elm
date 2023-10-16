e3175d542839bad565046d284757b65ad8dd6dfc
robertlisaru <lisaru.robert@gmail.com>
2023-10-16 13:02:00 +0300

    Clean P33 solution

--- src/Solutions/P33Coprimes.elm
+++ src/Solutions/P33Coprimes.elm
@@ -3,12 +3,13 @@ module Solutions.P33Coprimes exposing (coprimes)
 
 coprimes : Int -> Int -> Bool
 coprimes m n =
-    let
-        gcd a b =
-            if b == 0 then
-                abs a
-
-            else
-                gcd b (a |> remainderBy b)
-    in
     gcd m n == 1
+
+
+gcd : Int -> Int -> Int
+gcd a b =
+    if b == 0 then
+        abs a
+
+    else
+        gcd b (a |> remainderBy b)
