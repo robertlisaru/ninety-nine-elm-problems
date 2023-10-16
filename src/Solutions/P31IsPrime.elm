8dd824a55f6132a9e669161634dde55f1a617ef4
robertlisaru <lisaru.robert@gmail.com>
2023-10-16 12:42:12 +0300

    Update P31 isPrime solution

--- src/Solutions/P31IsPrime.elm
+++ src/Solutions/P31IsPrime.elm
@@ -7,7 +7,5 @@ isPrime : Int -> Bool
 isPrime n =
     (n > 1)
         && (List.range 2 (n |> toFloat |> sqrt |> floor)
-                |> List.filter (\d -> (n |> modBy d) == 0)
-                |> List.length
+                |> List.all (\d -> (n |> modBy d) /= 0)
            )
-        == 0
